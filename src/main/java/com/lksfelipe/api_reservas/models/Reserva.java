dae7870752940ea03e76fad37071f167b780f960
Lukas Felipe Mendes Farias <felipelukas@hotmail.com>
2026-08-03 06:07:20 -0300

    Configuração do ORM

--- src/main/java/com/lksfelipe/api_reservas/models/Reserva.java
+++ src/main/java/com/lksfelipe/api_reservas/models/Reserva.java
@@ -5,19 +5,40 @@ import java.time.LocalTime;
 
 import com.lksfelipe.api_reservas.models.enums.StatusReserva;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "reservas")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Reserva {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sala_id")
     private Sala sala;
+
     private LocalDate data;
+
     private LocalTime horaInicio;
+
     private LocalTime horaFim;
+
+    @Enumerated(value = EnumType.STRING)
     private StatusReserva status;
 }
--- src/main/java/com/lksfelipe/api_reservas/models/Sala.java
+++ src/main/java/com/lksfelipe/api_reservas/models/Sala.java
@@ -4,18 +4,35 @@ import java.util.List;
 
 import com.lksfelipe.api_reservas.models.enums.TipoSala;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "salas")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Sala {
-
+    
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
+    @OneToMany(mappedBy = "sala")
     private List<Reserva> reservas;
+
+    @Enumerated(value = EnumType.STRING)
     private TipoSala tipoSala;
 
 }
